55214f9d87956b5e16531170f86e6b8f2dad0d42
TuanBT <buithanhtuan55@gmail.com>
2023-11-01 18:56:15 +0700

    Update swap form

--- src/problem2/fancy-form/src/components/CurrencySwapForm/index.tsx
+++ src/problem2/fancy-form/src/components/CurrencySwapForm/index.tsx
@@ -2,7 +2,6 @@ import { useForm, Controller } from "react-hook-form";
 import "./styles.scss";
 import {
   Box,
-  FormControl,
   TextField,
   Autocomplete,
   Button,
@@ -11,16 +10,18 @@ import {
 import axios from "axios";
 import { useState, useEffect } from "react";
 import SwapVerticalCircleIcon from "@mui/icons-material/SwapVerticalCircle";
+import IconDefault from "../../assets/react.svg";
 
 type FormType = {
   amountToSend: number;
-  amountToReceive: number;
+  pricesFrom: any;
+  pricesTo: any;
 };
 
 const defaulPrices = {
   currency: "",
-  price: "",
-  data: "",
+  price: 0,
+  date: "",
 };
 
 export const CurrencySwapForm = () => {
@@ -38,17 +39,34 @@ export const CurrencySwapForm = () => {
         .get("https://interview.switcheo.com/prices.json")
         .then((res) => res.data)) as Models.CurrencyModel[];
 
-      setPrices(priceList);
+      setPrices(removeDuplicateItem(priceList));
     } catch (error) {
       console.log(error);
     }
   };
 
+  const removeDuplicateItem = (list: Models.CurrencyModel[]) => {
+    if (list && list?.length !== 0) {
+      const uniqueItem = Array.from(
+        new Set(list.map((item) => item.currency))
+      ).map((currency) => {
+        return list.find((item) => item.currency === currency);
+      }) as Models.CurrencyModel[];
+      return uniqueItem;
+    } else return list;
+  };
+
   const getPriceByCurrency = (currency: string) => {
-    prices.forEach((item) => {
-      if (item.currency === currency) return item.price;
-      else return null;
-    });
+    if (prices && currency) {
+      const priceItem = prices.find((item: Models.CurrencyModel) => {
+        if (item.currency.toLowerCase() === currency.toLowerCase()) {
+          return item;
+        }
+      });
+      return priceItem ? priceItem?.price : 0;
+    } else {
+      return 0;
+    }
   };
 
   const handleSwap = (
@@ -56,16 +74,16 @@ export const CurrencySwapForm = () => {
     fromCurrency: string,
     toCurrency: string
   ) => {
-    if (+getPriceByCurrency(fromCurrency)) {
-      const toValue = (
-        (fromAmount * +getPriceByCurrency(fromCurrency)) /
-        +getPriceByCurrency(toCurrency)
-      ).toFixed(2);
-      setReceiveValue(+toValue);
-      return (
-        (fromAmount * +getPriceByCurrency(fromCurrency)) /
-        +getPriceByCurrency(toCurrency)
-      ).toFixed(2);
+    if (
+      getPriceByCurrency(fromCurrency) &&
+      getPriceByCurrency(fromCurrency) !== 0
+    ) {
+      const toValue =
+        (fromAmount * getPriceByCurrency(fromCurrency)) /
+        getPriceByCurrency(toCurrency);
+
+      setReceiveValue(+toValue.toFixed(6));
+      getPriceByCurrency(fromCurrency);
     } else return 0;
   };
 
@@ -85,7 +103,7 @@ export const CurrencySwapForm = () => {
 
   return (
     <Box>
-      <FormControl onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Box
           sx={{
             backgroundImage: "linear-gradient(180deg, #1a2644, #0e0e2a)",
@@ -115,80 +133,97 @@ export const CurrencySwapForm = () => {
                 rules={{
                   required: "This is required",
                   pattern: {
-                    value: /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/,
-                    message: "Please enter number",
+                    value: /^[0-9]+$/,
+                    message: "Please enter valid number",
                   },
                 }}
-                render={() => (
+                render={({ field, fieldState }) => (
                   <TextField
                     size="medium"
                     InputProps={{
+                      inputProps: {
+                        min: 0,
+                      },
                       style: {
                         borderRadius: "10px",
                         padding: "0 10px",
                         border: "none",
                       },
                     }}
-                    type="number"
                     variant="standard"
                     className="text-field"
-                    required
-                    // value={email}
                     onChange={(e) => {
-                      setValue("amountToReceive", +e.target.value);
+                      setValue("amountToSend", +e.target.value);
+                      field.onChange(e);
                     }}
                     label="Amount to send"
                     placeholder="Enter amount"
+                    error={!!fieldState.error}
+                    helperText={fieldState.error?.message}
                   />
                 )}
               />
-              <Autocomplete
-                sx={{ width: 300 }}
-                options={
-                  prices?.map((item: Models.CurrencyModel) => ({
-                    label: item.currency,
-                    value: item.price,
-                    icon: `https://raw.githubusercontent.com/Switcheo/token-icons/main/tokens/${item.currency}.svg`,
-                  })) || []
-                }
-                onChange={(e, data: any) =>
-                  setFromCurrency({
-                    currency: data?.label,
-                    price: data?.value,
-                    date: "",
-                  })
-                }
-                autoHighlight
-                getOptionLabel={(option) => option.label}
-                className="currency-autocomplete"
-                renderOption={(props, option) => (
-                  <Box
-                    component="li"
-                    sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
-                    {...props}
-                  >
-                    <img
-                      loading="lazy"
-                      width="20"
-                      srcSet={`https://raw.githubusercontent.com/Switcheo/token-icons/main/tokens/${option.label}.svg`}
-                      src={`https://raw.githubusercontent.com/Switcheo/token-icons/main/tokens/${option.label}.svg`}
-                      alt={option.label}
+              <Controller
+                name="pricesFrom"
+                control={control}
+                rules={{
+                  required: "Please enter currency",
+                }}
+                render={({ field, fieldState }) => {
+                  return (
+                    <Autocomplete
+                      {...field}
+                      sx={{ width: 300 }}
+                      options={
+                        prices?.map((item: Models.CurrencyModel) => ({
+                          label: item.currency,
+                          value: item.price,
+                          icon: `https://raw.githubusercontent.com/Switcheo/token-icons/main/tokens/${item.currency}.svg`,
+                        })) || []
+                      }
+                      onChange={(_, data: any) => {
+                        setFromCurrency({
+                          currency: data?.label,
+                          price: data?.value,
+                          date: "",
+                        });
+                        field.onChange(data);
+                      }}
+                      autoHighlight
+                      getOptionLabel={(option) => option.label}
+                      isOptionEqualToValue={(option, value) =>
+                        option.label === value.label
+                      }
+                      className="currency-autocomplete"
+                      renderOption={(props, option) => (
+                        <Box
+                          component="li"
+                          sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+                          {...props}
+                        >
+                          <img
+                            loading="lazy"
+                            width="20"
+                            srcSet={option.icon || IconDefault}
+                            src={option.icon || IconDefault}
+                            alt={option.label}
+                          />
+                          {option.label}
+                        </Box>
+                      )}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          label="Choose a currency"
+                          variant="standard"
+                          fullWidth
+                          error={!!fieldState.error}
+                          helperText={fieldState.error?.message}
+                        />
+                      )}
                     />
-                    {option.label}
-                  </Box>
-                )}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Choose a currency"
-                    variant="standard"
-                    fullWidth
-                    // inputProps={{
-                    //   ...params.inputProps,
-                    //   autoComplete: "new-password", // disable autocomplete and autofill
-                    // }}
-                  />
-                )}
+                  );
+                }}
               />
             </Box>
           </Box>
@@ -208,78 +243,96 @@ export const CurrencySwapForm = () => {
             }}
           >
             <Box sx={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-              <Controller
-                control={control}
-                name="amountToReceive"
-                render={() => (
-                  <TextField
-                    size="medium"
-                    InputProps={{
-                      style: {
-                        borderRadius: "10px",
-                        padding: "0 10px",
-                        border: "none",
-                      },
-                      readOnly: true,
-                      disabled: true,
-                    }}
-                    value={receiveValue}
-                    variant="standard"
-                    className="text-field"
-                    label="Amount to receive"
-                  />
-                )}
+              <TextField
+                size="medium"
+                InputProps={{
+                  style: {
+                    borderRadius: "10px",
+                    padding: "0 10px",
+                    border: "none",
+                  },
+                  readOnly: true,
+                }}
+                aria-readonly
+                value={receiveValue}
+                variant="standard"
+                className="text-field"
+                label="Amount to receive"
               />
-              <Autocomplete
-                sx={{ width: 300 }}
-                options={
-                  prices?.map((item: Models.CurrencyModel) => ({
-                    label: item.currency,
-                    value: item.price,
-                    icon: `https://raw.githubusercontent.com/Switcheo/token-icons/main/tokens/${item.currency}.svg`,
-                  })) || []
-                }
-                autoHighlight
-                getOptionLabel={(option) => option.label}
-                className="currency-autocomplete"
-                renderOption={(props, option) => (
-                  <Box
-                    component="li"
-                    sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
-                    {...props}
-                  >
-                    <img
-                      loading="lazy"
-                      width="20"
-                      srcSet={`https://raw.githubusercontent.com/Switcheo/token-icons/main/tokens/${option.label}.svg`}
-                      src={`https://raw.githubusercontent.com/Switcheo/token-icons/main/tokens/${option.label}.svg`}
-                      alt={option.label}
+              <Controller
+                name="pricesTo"
+                control={control}
+                rules={{
+                  required: "Please enter currency",
+                }}
+                render={({ field, fieldState }) => {
+                  return (
+                    <Autocomplete
+                      {...field}
+                      sx={{ width: 300 }}
+                      options={
+                        prices?.map((item: Models.CurrencyModel) => ({
+                          label: item.currency,
+                          value: item.price,
+                          icon: `https://raw.githubusercontent.com/Switcheo/token-icons/main/tokens/${item.currency}.svg`,
+                        })) || []
+                      }
+                      onChange={(_, data: any) => {
+                        setToCurrency({
+                          currency: data?.label,
+                          price: data?.value,
+                          date: "",
+                        });
+                        field.onChange(data);
+                      }}
+                      autoHighlight
+                      getOptionLabel={(option) => option.label}
+                      isOptionEqualToValue={(option, value) =>
+                        option.label === value.label
+                      }
+                      className="currency-autocomplete"
+                      renderOption={(props, option) => (
+                        <Box
+                          component="li"
+                          sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+                          {...props}
+                        >
+                          <img
+                            loading="lazy"
+                            width="20"
+                            srcSet={option.icon || IconDefault}
+                            src={option.icon || IconDefault}
+                            alt={option.label}
+                          />
+                          {option.label}
+                        </Box>
+                      )}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          label="Choose a currency"
+                          variant="standard"
+                          fullWidth
+                          error={!!fieldState.error}
+                          helperText={fieldState.error?.message}
+                        />
+                      )}
                     />
-                    {option.label}
-                  </Box>
-                )}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Choose a currency"
-                    variant="standard"
-                    fullWidth
-                    // inputProps={{
-                    //   ...params.inputProps,
-                    //   autoComplete: "new-password", // disable autocomplete and autofill
-                    // }}
-                  />
-                )}
+                  );
+                }}
               />
             </Box>
           </Box>
-          <Box sx={{ width: "100%" }}>
-            <Button variant="contained" fullWidth sx={{ mt: 3, color: "#fff" }}>
-              Swap
-            </Button>
-          </Box>
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{ mt: 3, color: "#fff" }}
+            type="submit"
+          >
+            Swap
+          </Button>
         </Box>
-      </FormControl>
+      </form>
     </Box>
   );
 };
